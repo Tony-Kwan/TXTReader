eeaacf057019d193a86f2191ff60c404e79efa24
Tony-Kwan <15813669070@163.com>
2015-03-02 19:16:04 +0800

    temp

---
+++ Pods/KMCGeigerCounter/KMCGeigerCounter/KMCGeigerCounter.h
@@ -0,0 +1,25 @@
+//
+//  KMCGeigerCounter.h
+//  KMCGeigerCounter
+//
+//  Created by Kevin Conner on 10/21/14.
+//  Copyright (c) 2014 Kevin Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KMCGeigerCounter : NSObject
+
+// Set [KMCGeigerCounter sharedGeigerCounter].enabled = YES from -application:didFinishLaunchingWithOptions:
+@property (nonatomic, assign, getter = isEnabled) BOOL enabled;
+
+// Draws over the status bar. Set it manually if your own custom windows obscure it.
+@property (nonatomic, assign) UIWindowLevel windowLevel;
+
+@property (nonatomic, readonly, getter = isRunning) BOOL running;
+@property (nonatomic, readonly) NSInteger droppedFrameCountInLastSecond;
+@property (nonatomic, readonly) NSInteger drawnFrameCountInLastSecond; // -1 until one second of frames have been collected
+
++ (instancetype)sharedGeigerCounter;
+
+@end
